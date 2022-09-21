1a43417e81b941f9abcdcfff8d76581aa547bee3
joseong9 <jsg8825@gamil.com>
2022-09-21 12:28:46 +0900

    NodeJS

---
+++ 02.http/22.list.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const qs = require('querystring');
+const view = require('./view/index');
+
+http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    switch(pathname) {
+    case '/':
+        fs.readdir('data', (err, files)=>{
+            let list = '';
+            for (let file of files){
+                const title = file.substring(0, file.length-4);
+                list += `<li><h3><a href="/?id=${title}">${title}</a></h3></li>`
+            }
+            const html = view.index(list);
+            res.end(html);
+        });
+        break;
+    default:
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end();
+    }
+}).listen(3000, ()=> {
+    console.log('Server running at http://localhost:3000')
+})
--- 02.http/view/index.js
+++ 02.http/view/index.js
@@ -1,4 +1,4 @@
-exports.index = function() {
+exports.index = function(list) {
     return `
     <!DOCTYPE html>
 <html lang="ko">
@@ -11,14 +11,12 @@ exports.index = function() {
 <body style="margin: 35px;">
     <h1>웹 기술</h1>
     <ul>
-        <li><h3>CSS</h3></li>
-        <li><h3>HTML</h3></li>
-        <li><h3>JavaScript</h3></li>
+        ${list}
     </ul>
     <hr>
     <p>
         월드 와이드 웹이란
-        인터넷에 연결된 사용자들이 서로의 정보를 공유할 수 이쓴 공간을 의미합니다.
+        인터넷에 연결된 사용자들이 서로의 정보를 공유할 수 있는 공간을 의미합니다.
         간단히 줄여서 WWW나 W3라고 부릅니다.
     </p>
 </body>
