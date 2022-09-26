b8ee4d7e9ef27ca14d159792fdc434b71642334e
joseong9 <jsg8825@gmail.com>
2022-09-26 11:13:33 +0900

    NodeJS

--- 03.MySQL/12.조회html.js
+++ 03.MySQL/12.조회html.js
@@ -2,47 +2,45 @@ const fs = require('fs');
 const mysql = require('mysql');
 const config = require('./mysql.json');
 let html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Country Code</th>
-                <th>District</th>
-                <th>Population</th>
-            </tr>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Country Code</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
 `;
 
 const connection = mysql.createConnection(config);
 
-connection.connect();
+connection.connect();           // MySQL DB에 접속
 const sql = `SELECT * FROM city WHERE population > 9000000;`;
-connection.query(sql, function(err, rows, fields) {
+connection.query(sql, (err, rows, fields) => {
     if (err)
         throw err;
-    // console.log(rows);   rows : 배열 타입
     for (let row of rows) {
         let line = '<tr>';
-        line += `<td>${row.ID}</td><td>${row.Name}</td><td>${row.CountryCode}</td><td>${row.District}</td>${row.Population}</td>`
-        line += '<tr/>\n';
+        line += `<td>${row.ID}</td><td>${row.Name}</td><td>${row.CountryCode}</td><td>${row.District}</td><td>${row.Population}</td>`;
+        line += '</tr>\n';
         html += line;
     }
     html += `
-        </table>
-    </body>
-    </html>
-    `
-    fs.writeFile('12.table.html', html, err=> {
-        
-    })
-});
-connection.end();
+    </table>
+</body>
+</html>
+    `;
+    fs.writeFile('12.table.html', html, err => {
 
+    });
+});
+connection.end();               // MySQL DB 접속 해제
