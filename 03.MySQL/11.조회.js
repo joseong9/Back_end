2ce34e023fea1e8fcb819480ee12cd80afbecf15
joseong9 <jsg8825@gmail.com>
2022-09-26 10:43:59 +0900

    NodeJS

--- 03.MySQL/11.조회.js
+++ 03.MySQL/11.조회.js
@@ -1,11 +1,5 @@
 const mysql = require('mysql');
-config = {
-    host:'localhost',
-    user:'gangjuaischool',
-    password:'gangjuaischoolpath',
-    port:3306,
-    database: 'world'
-}
+const config = require('./mysql.json');
 
 const connection = mysql.createConnection(config);
 
@@ -14,7 +8,13 @@ const sql = `SELECT * FROM city WHERE population > 9000000;`;
 connection.query(sql, function(err, rows, fields) {
     if (err)
         throw err;
-    console.log(rows);
-    console.log(fields);
+    // console.log(rows);   rows : 배열 타입
+    for (let row of rows) {
+        const str = `${row.ID}\t${row.Name}\t${row.CountryCode}\t${row.District}\t${row.Population}`;
+        console.log(str);
+    }
+
+    // console.log(fields);
 });
 connection.end();
+
