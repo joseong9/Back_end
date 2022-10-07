56954abd305d2c969b8647ecb000d40b0c8022e9
joseong9 <jsg8825@gmail.com>
2022-10-07 16:46:56 +0900

    save

--- 221004_1007/1006-1007/cluster/cluster.js
+++ 221004_1007/1006-1007/cluster/cluster.js
@@ -3,27 +3,27 @@ const http = require('http');
 const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-console.log(`마스터 프로세스 아이디: ${process.pid}`);
+    console.log(`마스터 프로세스 아이디: ${process.pid}`);
 // CPU 개수만큼 워커를 생산
-for (let i = 0; i < numCPUs; i += 1) {
-cluster.fork();
-}
+    for (let i = 0; i < numCPUs; i += 1) {
+        cluster.fork();
+    }
 // 워커가 종료되었을 때
-cluster.on('exit', (worker, code, signal) => {
-console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
-console.log('code', code, 'signal', signal);
-cluster.fork();
-});
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
+        console.log('code', code, 'signal', signal);
+        cluster.fork();
+    });
 } else {
 // 워커들이 포트에서 대기
-http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-res.write('<h1>Hello Node!</h1>');
-res.end('<p>Hello Cluster!</p>');
-setTimeout(() => { // 워커 존재를 확인하기 위해 1초마다 강제 종료
-process.exit(1);
-}, 1000);
-}).listen(8086);
+    http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.write('<h1>Hello Node!</h1>');
+        res.end('<p>Hello Cluster!</p>');
+        setTimeout(() => { // 워커 존재를 확인하기 위해 1초마다 강제 종료
+        process.exit(1);
+    }, 1000);
+    }).listen(8086);
 
-console.log(`${process.pid}번 워커 실행`);
+    console.log(`${process.pid}번 워커 실행`);
 }
