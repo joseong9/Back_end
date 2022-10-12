80fff97be3e3d27eded04c82d5cb9854e027250c
joseong9 <jsg8825@gmail.com>
2022-10-12 10:13:47 +0900

    save

--- 22.10.11/app2.js
+++ 22.10.11/app2.js
@@ -57,9 +57,11 @@ app.post('/upload', upload.single('image'), (req, res) => {
 app.get('/', (req, res, next) => {
   console.log('GET / 요청에서만 실행됩니다.');
   next();
-}, (req, res) => {
-  throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+},(req, res, next) => {
+//    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+    next();
 });
+
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send(err.message);
