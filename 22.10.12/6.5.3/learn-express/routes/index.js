80def45ec0b2f26bf07003516b18b334327d11b1
joseong9 <jsg8825@gmail.com>
2022-10-13 17:17:30 +0900

    save

--- 22.10.12/6.5.3/learn-express/routes/index.js
+++ 22.10.12/6.5.3/learn-express/routes/index.js
@@ -4,7 +4,23 @@ const router = express.Router();
 
 // GET / 라우터
 router.get('/', (req, res) => {
-  res.send('Hello, Express');
+  res.render('index', {title: 'hi'});
 });
 
 module.exports = router;
+
+// router.get('/', (req, res) => {
+//   res.send('GET /abc');
+// });
+
+// router.post('/', (req, res) => {
+//   res.send('POST /abc');
+// });
+
+// router.route('/abc')
+//   .get((req, res) => {
+//     res.send('GET /abc');
+//   })
+//   .post((req, res) => {
+//     res.send('POST /abc');
+//   })
